da89dc8eeed9b93a72e7a180e5f99f39ffc99881
oliver.bannister <oliver.bannister@gamesys.co.uk>
2022-01-28 18:05:29 +0000

    add clickable links

--- src/components/socialMedia/SocialMedia.tsx
+++ src/components/socialMedia/SocialMedia.tsx
@@ -1,17 +1,23 @@
 import './SocialMedia.css';
 import { IconButton } from '@mui/material';
 import { Instagram, Twitter } from '@mui/icons-material';
+import { useCallback } from 'react';
 
 function SocialMedia() {
+
+    const onClick = useCallback(() => {
+        window.open('https://www.instagram.com/olliebannister/');
+    }, [])
+
     return (
         <div className='social-media-container'>
-            <IconButton>
+            <IconButton onClick={onClick}>
                 <Instagram fontSize='large'/>
             </IconButton>
-            <IconButton>
+            <IconButton onClick={onClick}>
                 <Twitter fontSize='large'/>
             </IconButton>
-            <IconButton>
+            <IconButton onClick={onClick}>
                 <img className='social-media-link' alt='instagram' src='discord-icon.jpg'></img>
             </IconButton>
         </div>
